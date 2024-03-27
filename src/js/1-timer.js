96a78d080825da5c9da010f037e34a3d75e66fff
YanaKorniichuk25 <149394343+YanaKorniichuk25@users.noreply.github.com>
2024-03-27 14:25:20 +0100

    step 2

---
+++ src/js/1-timer.js
@@ -0,0 +1,6 @@
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
